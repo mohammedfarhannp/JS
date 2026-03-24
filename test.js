9c9a89825ecd4a6190c39987c5596b30a82f48da
mohammedfarhannp <m.farhan.np@gmail.com>
2026-03-24 20:09:00 +0530

    Learning Variables!

---
+++ test.js
@@ -0,0 +1,8 @@
+// Declaration and Definition before using // Works
+var x = 10
+console.log(x)
+
+// Declaration and Definition after using // UnDefined Output but no error
+console.log(y)
+var y = 20
+
