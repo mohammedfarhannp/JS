ba0c4027e2ed0ebd576f0206dec8f135aa26e775
mohammedfarhannp <m.farhan.np@gmail.com>
2026-03-25 19:21:57 +0530

    Learning Functions!

--- test.js
+++ test.js
@@ -1,11 +1,8 @@
-// Conditional Statements
-var x = 12 // Try changing Values
-var y = 11
-
-if (x > y) {
-    console.log("X Greater than Y")
-} else if (y == x) {
-    console.log("X is Equal to Y")
-} else {
-    console.log("Y Greater than X")
+// Function Example
+function example() {
+    console.log("Example Funcion!")
 }
+
+
+example()
+example()
