25fc08ee126697dc646016238119f522deb22f0f
mohammedfarhannp <m.farhan.np@gmail.com>
2026-03-25 19:25:43 +0530

    Learning Functions

---
+++ Functions.js
@@ -0,0 +1,13 @@
+// Function Example
+function example() {
+    console.log("Example Funcion!")
+}
+
+// Add Function
+// Unlike C language you don't have to specify it to be a variable or it's type for parameter
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(7, 7))
+
