300fa4c3976670d3e93a33af0ec4b5030b2d8acf
mohammedfarhannp <m.farhan.np@gmail.com>
2026-03-29 20:41:03 +0530

    Learning Objects

---
+++ Objects.js
@@ -0,0 +1,29 @@
+// Objects
+// Similar to Python Dictionaries
+
+var Object_Car = { "Name": "Totoyo", "Model": "U891", "Color": "Vanta Black", "Owner": "BATTMAAN" }
+
+console.log(Object_Car)
+console.log(Object_Car["Name"])
+
+
+// loop through Object
+// Method 1
+for (let key in Object_Car) {
+    console.log(key, Object_Car[key])
+}
+
+// Method 2
+Object.keys(Object_Car).forEach( key => {
+    console.log(key, Object_Car[key])
+});
+
+// Method 3
+Object.values(Object_Car).forEach(value => {
+    console.log(value)
+})
+
+// Method 4
+Object.entries(Object_Car).forEach(([key, value]) => {
+    console.log(key, value)
+})
